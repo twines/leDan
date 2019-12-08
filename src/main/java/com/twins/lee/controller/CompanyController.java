e71c819f249c0b644e373e804f483ac68a61c397
1355081829@qq.com <1355081829aA>
2019-12-08 16:09:27 +0800

    完善企业信息

--- src/main/java/com/twins/lee/controller/CompanyController.java
+++ src/main/java/com/twins/lee/controller/CompanyController.java
@@ -1,8 +1,14 @@
 package com.twins.lee.controller;
 
+import com.twins.lee.response.Response;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/company")
@@ -10,5 +16,12 @@ public class CompanyController {
     @GetMapping("/auth")
     public String auth() {
         return "/company/auth";
+    }
+
+    @PostMapping("/doAuth")
+    @ResponseBody
+    public Map<String, Object> aoAuth() {
+        Map<String, Object> map = new HashMap<>();
+        return Response.success(map);
     }
 }
