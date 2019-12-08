d166598f4bf0360e51b3b9114ebc9378a7a1e360
1355081829@qq.com <1355081829aA>
2019-12-08 13:15:58 +0800

    贸易单据上传

--- src/main/java/com/twins/lee/controller/ShippingController.java
+++ src/main/java/com/twins/lee/controller/ShippingController.java
@@ -1,9 +1,15 @@
 package com.twins.lee.controller;
 
+import com.twins.lee.common.CompanyTool;
+import com.twins.lee.entity.Company;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/shipping")
@@ -11,5 +17,14 @@ public class ShippingController {
     @GetMapping("add")
     public String index() {
         return "/shipping/add";
+    }
+
+    @PostMapping("add")
+    @ResponseBody
+    public Map<String, Object> add() {
+        Map<String, Object> map = new HashMap<>();
+        Company company = CompanyTool.getCompany();
+        company.getId();
+        return map;
     }
 }
