25dd458c683f3fb2a328efb92d5fe6e13234145c
lee <994158307@qq.com>
2019-12-08 12:59:31 +0800

    实现文件文件上传（目录按照uuid+session方式存储）

--- src/main/java/com/twins/lee/config/ResourceConfig.java
+++ src/main/java/com/twins/lee/config/ResourceConfig.java
@@ -1,8 +1,14 @@
 package com.twins.lee.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * @Authoe hanyun
@@ -11,9 +17,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  **/
 @Configuration
 public class ResourceConfig implements WebMvcConfigurer {
+
+    @Value("${twins.staticAccessPath}")
+    private String staticAccessPath;
+    @Value("${twins.uploadFolder}")
+    private String uploadFolder;
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize(DataSize.parse("5MB")); //KB,MB
+        /// 设置总上传数据总大小
+//        factory.setMaxRequestSize(DataSize.parse("300MB"));
+        return factory.createMultipartConfig();
+    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //第一个方法设置访问路径前缀，第二个方法设置资源路径
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+
+//        浏览上传的文件
+        registry.addResourceHandler(staticAccessPath).addResourceLocations("file:" + uploadFolder);
+
     }
 }
---
+++ src/main/java/com/twins/lee/controller/UploadController.java
@@ -0,0 +1,144 @@
+package com.twins.lee.controller;
+
+import com.twins.lee.response.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+
+//@SessionAttributes(value={"CURR_USER"},types={User.class})
+@Controller
+@RequestMapping("/file")
+public class UploadController {
+
+
+    @Value("${twins.uploadFolder}")
+    private String docLocation;
+
+    @RequestMapping("/upload")
+    @ResponseBody
+    public Map upload(@RequestParam("file") MultipartFile file, HttpSession session,
+                      @RequestParam(value = "needOCR", required = false, defaultValue = "false") boolean needOcr,
+                      @RequestParam(value = "needQr", required = false, defaultValue = "false") boolean needQr) {
+
+        String sessionId = session.getId();
+
+        // 获取文件名
+        String fileName = file.getOriginalFilename();
+
+        // 获取文件的后缀名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+
+
+        try {
+
+            // 文件上传路径
+            String filePath = null;
+            filePath = fileDestPath();
+            if (docLocation != null) {
+                filePath = docLocation;
+            }
+            //用户目录
+            String userDir = UUID.randomUUID().toString() + "/" + sessionId;
+
+            // 解决中文问题，liunx下中文路径，图片显示问题
+            fileName = UUID.randomUUID() + suffixName;
+            String fileUrl = "/" + userDir + "/" + fileName;
+            filePath = filePath + fileUrl;
+            File dest = new File(filePath);
+
+            // 检测是否存在目录
+            if (!dest.getParentFile().exists()) {
+                dest.getParentFile().mkdirs();
+            }
+
+            file.transferTo(dest);
+            Map result = null;
+            if (needOcr && needQr == false) {
+//                result = new UploadResult(fileUrl, ocrReco(fileUrl), null);
+            } else if (needQr && needOcr == false) {
+//                result = new UploadResult(fileUrl, null, qrReco(fileUrl));
+            } else if (needQr && needOcr) {
+//                String ocr = ocrReco(fileUrl);
+//                String qr = qrReco(fileUrl);
+//                result = new UploadResult(fileUrl, ocr, qr);
+            } else {
+//                Map value = new HashMap();
+//                value.put("value", fileUrl);
+//                result = Response.success(value);
+            }
+            Map value = new HashMap();
+            value.put("value", fileUrl);
+            return result = Response.success(value);
+        } catch (Exception e) {
+            return Response.error("系统错误:" + e.getLocalizedMessage());
+        }
+    }
+
+//    protected String qrReco(String imgPath) throws IOException, NotFoundException {
+//        String destPath = docLocation + imgPath;
+//
+//        BufferedImage image;
+//        image = ImageIO.read(new File(destPath));
+//        if (image == null) {
+//            return null;
+//        }
+//        BufferedImageLuminanceSource source = new BufferedImageLuminanceSource(image);
+//        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+//        com.google.zxing.Result result;
+//        Hashtable<DecodeHintType, Object> hints = new Hashtable<DecodeHintType, Object>();
+//        hints.put(DecodeHintType.CHARACTER_SET, CHARSET);
+//        result = new MultiFormatReader().decode(bitmap, hints);
+//
+//        String resultStr = result.getText();
+//
+//        return resultStr;
+//    }
+//
+//    private String ocrReco(String imgPath) {
+//        String sysPath = fileDestPath();
+//        String tranPath = sysPath;
+//        String destPath = sysPath + "/static" + imgPath;
+//        if (docLocation != null) {
+//            destPath = docLocation + imgPath;
+//        }
+//        File imageFile = new File(destPath);
+//        ITesseract instance = new Tesseract();
+//        instance.setDatapath(tranPath);
+//        instance.setLanguage("chi_sim");
+////        instance.setLanguage("eng");
+//
+//        try {
+//            String result = instance.doOCR(imageFile).replace(" ", "");
+//
+//            return result;
+//        } catch (TesseractException e) {
+//            return null;
+//        }
+//    }
+
+    private String fileDestPath() {
+        String filePath = null;
+        try {
+            filePath = ResourceUtils.getFile("classpath:").getAbsolutePath();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return filePath;
+    }
+
+
+}
