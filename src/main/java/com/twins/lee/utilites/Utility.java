61f88ef6ecd58ac06594dc6971cb16577df87909
sk <lylapp@163.com>
2019-12-17 22:47:18 +0800

    PDF 换换图片与 识别图片中的二维码

--- src/main/java/com/twins/lee/utilites/Utility.java
+++ src/main/java/com/twins/lee/utilites/Utility.java
@@ -1,5 +1,11 @@
 package com.twins.lee.utilites;
 
+import cn.hutool.extra.qrcode.BufferedImageLuminanceSource;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.common.HybridBinarizer;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.shiro.SecurityUtils;
@@ -11,9 +17,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static org.apache.catalina.manager.Constants.CHARSET;
 
 public class Utility {
 
@@ -70,6 +76,83 @@ public class Utility {
             e.printStackTrace();
         }
         return list;
+    }
+
+    public static void deleteAllFilesOfDir(File path) {
+        if (null != path) {
+            if (!path.exists())
+                return;
+            if (path.isFile()) {
+                boolean result = path.delete();
+                int tryCount = 0;
+                while (!result && tryCount++ < 10) {
+                    System.gc(); // 回收资源
+                    result = path.delete();
+                }
+            }
+            File[] files = path.listFiles();
+            if (null != files) {
+                for (int i = 0; i < files.length; i++) {
+                    deleteAllFilesOfDir(files[i]);
+                }
+            }
+            path.delete();
+        }
+    }
+
+    public static boolean deleteFile(String pathname) {
+        boolean result = false;
+        File file = new File(pathname);
+        if (file.exists()) {
+            file.delete();
+            result = true;
+            System.out.println("文件已经被成功删除");
+        }
+        return result;
+    }
+
+    public static String qrReco(String filePath) throws IOException, NotFoundException {
+        String destPath = filePath;
+        String[] component = destPath.split(".");
+        int suffixIndex = component.length - 1;
+        String extension = component[suffixIndex];
+
+        String result = null;
+        if (extension.toLowerCase() == "pdf") {
+            List<String> paths = pdfToImagePath(String.join("/", Arrays.copyOf(component, component.length - 1)));
+            for (String path : paths
+            ) {
+                String tmpResult = qrResut(path);
+                deleteFile(path);
+                if (tmpResult != null) {
+                    result = tmpResult;
+                    break;
+                }
+            }
+
+        } else {
+            result = qrResut(destPath);
+        }
+
+
+        return result;
+    }
+
+    private static String qrResut(String destPath) throws IOException, NotFoundException {
+        BufferedImage image;
+        image = ImageIO.read(new File(destPath));
+        if (image == null) {
+            return null;
+        }
+        BufferedImageLuminanceSource source = new BufferedImageLuminanceSource(image);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        com.google.zxing.Result result;
+        Hashtable<DecodeHintType, Object> hints = new Hashtable<DecodeHintType, Object>();
+        hints.put(DecodeHintType.CHARACTER_SET, CHARSET);
+        result = new MultiFormatReader().decode(bitmap, hints);
+
+        String resultStr = result.getText();
+        return resultStr;
     }
 
 
